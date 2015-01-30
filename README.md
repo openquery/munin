6069740c220065acdbcf460300906121b82486ef
Daniel Black <grooverdan@users.sourceforge.net>
2015-01-30 11:07:21 +1100

    manage from upstream. Install upstream munin-asyncd in /usr/sbin

--- README.md
+++ README.md
@@ -3,10 +3,14 @@ munin
 
 Here are a set of Open Query customisations for Munin.
 
-munin-asyncd has been altered to run strictly on the interval relative to the
-wall clock rather than the async starting time.
+Elements from our contributions upstream that we pull back to the current version:
 
-Other elements here are customisations that we intend to contribute back.
+munin-asyncd has been altered to run strictly on the interval relative to the
+wall clock rather than the async starting time. (upstream version 2.1.10+/2.0.24+)
+
+mysql\_ plugin - lots of added graphs
+
+numa\_ plugin - new
 
 See offical codebase:
 https://github.com/munin-monitoring/munin
@@ -19,7 +23,6 @@ git clone https://github.com/openquery/munin.git /etc/munin/openquery
 If using Debian (and I assume Ubuntu too)
 create /etc/default/munin-asyncd and set it with the following:
 
-DAEMON=/etc/munin/openquery/$NAME
 DAEMON_ARGS="--spooldir /var/lib/munin-async"
 
 After that /etc/init.d/munin-async will work as normal
--- link.sh
+++ link.sh
@@ -1,98 +1,23 @@
 #!/bin/bash
 
-# grep '^\$graphs{' mysql_ | cut -f 2 -d { | cut -f 1 -d } | xargs -n1 -I ^ echo mysql_^ \\
+# --no-check-certificate as wget 1.12-1.8.el6 didn't recognise *.domain in SubjectAltName
 
-if pidof mysqld
-then
-  rm -f /etc/munin/plugins/mysql_files_tables
-  for m in  \
-mysql_bin_relay_log \
-mysql_commands \
-mysql_connections \
-mysql_files \
-mysql_tables \
-mysql_table_definitions \
-mysql_innodb_bpool \
-mysql_innodb_bpool_act \
-mysql_innodb_bpool_internal_breakdown \
-mysql_innodb_insert_buf \
-mysql_innodb_adaptive_hash \
-mysql_innodb_io \
-mysql_innodb_io_pend \
-mysql_innodb_log \
-mysql_innodb_rows \
-mysql_innodb_semaphores \
-mysql_innodb_tnx \
-mysql_performance \
-mysql_innodb_srv_master_thread \
-mysql_innodb_queries \
-mysql_innodb_read_views \
-mysql_innodb_descriptors \
-mysql_myisam_indexes \
-mysql_network_traffic \
-mysql_qcache \
-mysql_qcache_mem \
-mysql_max_mem \
-mysql_replication \
-mysql_select_types \
-mysql_slow \
-mysql_sorts \
-mysql_table_locks \
-mysql_tmp_tables \
-mysql_rows \
-mysql_handler_read \
-mysql_handler_transaction \
-mysql_handler_write \
-mysql_handler_tmp \
-mysql_execution \
-mysql_icp \
-mysql_mrr \
-    ;
-  do
-    rm -f "/etc/munin/plugins/${m}"
-    ln -s /etc/munin/openquery/mysql_ "/etc/munin/plugins/${m}"
-  done
-  if [ -e /usr/lib*/galera/libgalera_smm.so ]; then
+wget --no-check-certificate https://raw.githubusercontent.com/munin-monitoring/munin/devel/plugins/node.d/mysql_.in -O - | sed -e 's:@@PERL@@:/usr/bin/perl:' > /etc/munin/openquery/mysql_ || exit 1
+wget --no-check-certificate https://raw.githubusercontent.com/munin-monitoring/munin/devel/plugins/node.d.linux/numa_.in -O - | sed -e 's:@@GOODSH@@:/bin/bash:' > /etc/munin/openquery/numa_ || exit 1
 
-    for m in  \
-mysql_wsrep_cluster_status \
-mysql_wsrep_cluster_size \
-mysql_wsrep_local_state \
-mysql_wsrep_transactions \
-mysql_wsrep_writesets \
-mysql_wsrep_writesetbytes \
-mysql_wsrep_errors \
-mysql_wsrep_queue \
-mysql_wsrep_concurrency \
-mysql_wsrep_flow \
-mysql_wsrep_flow_paused \
-mysql_wsrep_flow_paused_ns \
-mysql_wsrep_distance \
-      ;
-    do
-      rm -f "/etc/munin/plugins/${m}"
-      ln -s /etc/munin/openquery/mysql_ "/etc/munin/plugins/${m}"
-    done
-  fi
+cp /usr/sbin/munin-asyncd /usr/sbin/munin-asyncd.orig
+wget --no-check-certificate https://raw.githubusercontent.com/munin-monitoring/munin/devel/node/_bin/munin-asyncd.in -O - | sed -e 's:@@PERL@@:/usr/bin/perl:' -e 's:@@SPOOLDIR@@:/var/lib/munin/spool:' > /usr/sbin/munin-asyncd || exit 1
 
+chown root: /usr/sbin/munin-asyncd
+chmod a+x /usr/sbin/munin-asyncd /etc/munin/openquery/mysql_ /etc/munin/openquery/numa_
 
-fi
-
-pushd /etc/munin/openquery/conf
-for conf in * ; do
-  [ -e "/etc/munin/plugin-conf.d/${conf}" ] || \
-    ln -s "/etc/munin/openquery/conf/${conf}" /etc/munin/plugin-conf.d/
-done
-popd
-
+munin-node-configure  --libdir /etc/munin/openquery/ --shell | sh
 
 if selinuxenabled; then
 # Centos 6.4 is munin_services_plugin_exec_t
   semanage fcontext  -a  -t services_munin_plugin_exec_t  /etc/munin/openquery/mysql_ || semanage fcontext -a -t munin_services_plugin_exec_t "/etc/munin/openquery/.*_"
-  semanage fcontext  -a  -t munin_etc_t  "/etc/munin/openquery/conf(/.*)?"
-  semanage fcontext  -a  -t munin_exec_t  "/etc/munin/openquery/munin-asyncd"
 
-
+  restorecon -rv /usr/sbin/munin-asyncd
   restorecon -rv /etc/munin/openquery/
   pushd /etc/munin/openquery
   make -f /usr/share/selinux/devel/Makefile
